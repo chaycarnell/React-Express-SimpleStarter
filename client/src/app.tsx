eb51ef104568edcf719b3ec08a481a5e022c4257
Chay Carnell <chaycarnell@gmail.com>
2024-03-28 19:04:48 +1000

    chore(linting): update linting scripts and fix minor linting issues

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [serverMessage, setServerMessage] = useState(undefined);
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,15 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/rest": "http://localhost:4001",
+      '/rest': 'http://localhost:4001',
     },
   },
   plugins: [react()],
   build: {
-    outDir: "../public",
+    outDir: '../public',
   },
 });
